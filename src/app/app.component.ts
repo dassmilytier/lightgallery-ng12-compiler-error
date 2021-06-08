068b325514437d20f559e8bd2654807d19ac6bf8
MichaelLipaczewski <nEMb&cBmg*8M*OKu>
2021-06-08 11:51:34 +0200

    vanilla angular 12 + lightgallery

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import Zoom from "lightgallery/plugins/zoom";
+import Video from "lightgallery/plugins/video";
+import { InitDetail } from "lightgallery/lg-events";
+import { LightGallery } from "lightgallery/lightgallery";
 
 @Component({
   selector: 'app-root',
@@ -7,4 +11,14 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'lightgallery-ng12-compiler-error';
+  settings = {
+    counter: false,
+    download: false,
+    plugins: [Zoom, Video],
+  };
+  private lightGallery!: LightGallery;
+
+  galleryOnInit(detail: InitDetail): void {
+    this.lightGallery = detail.instance;
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,14 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { LightgalleryModule } from "lightgallery/angular";
 
 @NgModule({
   declarations: [
     AppComponent
   ],
-  imports: [
-    BrowserModule
-  ],
+    imports: [
+        BrowserModule,
+        LightgalleryModule
+    ],
   providers: [],
   bootstrap: [AppComponent]
 })
